0ad7fa053a9d3298d1b1413ed3b9e560de579462
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2024-02-05 19:08:28 +0100

    5automatic users wip

--- utils/autoNewReleases.js
+++ utils/autoNewReleases.js
@@ -1,5 +1,7 @@
 const spotifyApi = require("../api/api")
 
+
+
 const cleanSongsArrFromApi = (songsArray) =>{
 
     const cleanSongsArr = songsArray.map((song) => {
@@ -15,15 +17,27 @@ const cleanSongsArrFromApi = (songsArray) =>{
     return cleanSongsArr;
 }
 
+const apiCallforSongData = async(title)=>{
+    const song = await spotifyApi.searchTracks(title)
+    return song.body.tracks.items;
+}
+
 //This function will automatically publish 5 songs with fake users
 const getNewReleases = async() =>{
     try {
         const data = await spotifyApi.getNewReleases({ limit: 5, offset: 0 })
-        const dataSongsToPublished = data.body.albums.items;
-        const song = await spotifyApi.searchTracks(dataSongsToPublished[0].name)
-        console.log("SONG", song.body.tracks.items)
-        // console.log("DATA", data.body.albums.items);
-        const cleanSongsArr = cleanSongsArrFromApi(dataSongsToPublished);
+        const newReleasesData = data.body.albums.items;
+        
+        const newReleasesArr = data.body.albums.items;
+
+        const newReleasesNames = newReleasesArr.map((song) => song.name)
+        const songsToPublish = await Promise.all(
+            newReleasesNames.map(async(title)=>{
+            const song = await apiCallforSongData(title)
+            return song[0];
+        })
+        )
+        const cleanSongsArr = cleanSongsArrFromApi(songsToPublish);
         console.log(cleanSongsArr)
     } catch (error) {
         console.log(error);
